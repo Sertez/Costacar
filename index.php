32fac0faf37ba27dfad7855364e5cb17423943b1
Daniel Donado <37049813+Sertez@users.noreply.github.com>
2020-02-29 13:32:57 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Costacar</title>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css.css">
+  
+</head>
+
+<body>
+  <div id="">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" >
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active" style="height: 100vh">
+          <img src="carro.jpg" class="d-block d-sm-block w-100 " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Renta los mejores autos</h5>
+            <p>Autos en perfecto estado y en multiples modelos</p>
+          </div>
+        </div>
+        <div class="carousel-item" style="height: 100vh">
+          <img src="colombia.jpg" class="d-block w-100 " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Viaja por toda Colombia</h5>
+            <p>Atrévete a explorar todo el territorio colombiano</p>
+          </div>
+        </div>
+        <div class="carousel-item" style="height: 100vh">
+          <img src="dinero.jpg" class="d-block w-100 " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Ahorra dinero</h5>
+            <p>Nosotros cubrimos combustible, seguro, parqueo y mantenimiento</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+  <div>
+    <nav class="navbar navbar-dark" style="background-color: #04B431;">
+      <a class="navbar-brand" href="#">
+        <img src="Costacar.png" width="150" height="40" class="d-inline-block align-top" alt="">
+      </a>
+      <a class="navbar-brand" href="#">
+        hola
+      </a>
+      <a class="navbar-brand" href="#">
+        hola
+      </a>
+      <a class="navbar-brand" href="#">
+        hola
+      </a>
+    </nav>
+  </div>
+  <div id="navbar">
+    hola mundo
+  </div>
+</body>
+
+</html>
