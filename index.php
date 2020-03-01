7c5f226403840bd88adffcc9a7a56a85ef2ae3b0
Daniel Donado <37049813+Sertez@users.noreply.github.com>
2020-03-01 01:50:07 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -8,16 +8,16 @@
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <!-- JS -->
+  <link rel="stylesheet" href="css.css">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="css.css">
-  
+
 </head>
 
 <body>
   <div id="">
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel" >
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
@@ -57,24 +57,24 @@
     </div>
   </div>
   <div>
-    <nav class="navbar navbar-dark" style="background-color: #04B431;">
-      <a class="navbar-brand" href="#">
-        <img src="Costacar.png" width="150" height="40" class="d-inline-block align-top" alt="">
-      </a>
-      <a class="navbar-brand" href="#">
-        hola
-      </a>
-      <a class="navbar-brand" href="#">
-        hola
-      </a>
-      <a class="navbar-brand" href="#">
-        hola
-      </a>
-    </nav>
-  </div>
-  <div id="navbar">
-    hola mundo
-  </div>
+    <div style="background-image:url(verde.jpg)">
+      <div style="margin-left:27vw; ">
+        <img src="Costacar.png" style="height: 25vh; width:46vw; "  alt="">
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md">
+
+        </div>
+        <div class="col-md">
+
+        </div>
+        <div class="col-md">
+
+        </div>
+      </div>
+    </div>
 </body>
 
 </html>
