b92bc54c3201550c251157abc02642a687360f33
DvC99 <39929176+DvC99@users.noreply.github.com>
2020-03-02 15:17:35 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <!-- JS -->
-  <link rel="stylesheet" href="css.css">
+  <link rel="stylesheet" href="css/css.css">
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -16,7 +16,7 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="background-image:url(verde.jpg); opacity:0.9">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top" style="background-image:url(jpg/verde.jpg); opacity:0.9">
     <a class="navbar-brand" href="#" id="blanco">Costacar</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -52,21 +52,21 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active" style="height: 100vh">
-          <img src="carro.jpg" class="d-block d-sm-block w-100 " alt="...">
+          <img src="jpg/carro.jpg" class="d-block d-sm-block w-100 " alt="...">
           <div class="carousel-caption d-none d-md-block">
             <h5>Renta los mejores autos</h5>
             <p>Autos en perfecto estado y en multiples modelos</p>
           </div>
         </div>
         <div class="carousel-item" style="height: 100vh">
-          <img src="colombia.jpg" class="d-block w-100 " alt="...">
+          <img src="jpg/colombia.jpg" class="d-block w-100 " alt="...">
           <div class="carousel-caption d-none d-md-block">
             <h5>Viaja por toda Colombia</h5>
             <p>Atrévete a explorar todo el territorio colombiano</p>
           </div>
         </div>
         <div class="carousel-item" style="height: 100vh">
-          <img src="dinero.jpg" class="d-block w-100 " alt="...">
+          <img src="jpg/dinero.jpg" class="d-block w-100 " alt="...">
           <div class="carousel-caption d-none d-md-block">
             <h5>Ahorra dinero</h5>
             <p>Nosotros cubrimos combustible, seguro, parqueo y mantenimiento</p>
@@ -83,12 +83,12 @@
       </a>
     </div>
   </div>
-  <div style="background-image:url(verde.jpg);">
+  <div style="background-image:url(jpg/verde.jpg);">
     <div style="margin-left:27vw; ">
-      <img src="Costacar.png" style="height: 25vh; width:46vw; " alt="">
+      <img src="png/Costacar.png" style="height: 25vh; width:46vw; " alt="">
     </div>
   </div>
-  <div style="background-image: url(fondo.png);">
+  <div style="background-image: url(png/fondo.png);">
     <div class="container">
       <div class="row">
         <div class="col-md" id="texto">
@@ -109,13 +109,13 @@
     <div class="container">
       <div class="row">
         <div class="col-sm" style="text-align: center">
-          <img src="membresia.png">
+          <img src="png/membresia.png">
         </div>
         <div class="col-sm" style="text-align: center">
-          <img src="rentar.png">
+          <img src="png/rentar.png">
         </div>
         <div class="col-sm" style="text-align: center">
-          <img src="devolver.png">
+          <img src="png/devolver.png">
         </div>
       </div>
     </div>
@@ -134,7 +134,7 @@
     </div>
     <p> <br><br></p>
     <div class="footer" style="width:100wh">
-      <div style="background-image:url(verde.jpg)" >
+      <div style="background-image:url(jpg/verde.jpg)" >
         <!-- Estas letras tienen que ir centradas y en la mitad de la imagen verdad, encima de la imagen verde, que no ocupen mucho espacio. Si te das cuenta debajo de la imagen verde hay un espacio en blanco que no se como quitar-->
         <p><br></p>
         <h6 id="blanco" style="text-align:center">Contactanos</h6>
