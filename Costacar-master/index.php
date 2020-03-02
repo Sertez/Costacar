4c113cc78f4471bd0e19605e4fd8f8407d49df81
Santiago26009 <39531781+Santiago26009@users.noreply.github.com>
2020-03-01 21:30:47 -0500

    Add files via upload

---
+++ Costacar-master/index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Costacar</title>
+  <!-- Bootstrap -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <!-- JS -->
+  <link rel="stylesheet" href="css.css">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <div style="background-image:url(verde.jpg)">
+      <div style="margin-left:27vw; ">
+        <img src="Costacar.png" style="height: 25vh; width:46vw; "  alt="">
+      </div>
+    </div>
+  <div id="">
+    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active" style="height: 100vh">
+          <img src="carro.jpg" class="d-block d-sm-block w-100 " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Renta los mejores autos</h5>
+            <p>Autos en perfecto estado y en multiples modelos</p>
+          </div>
+        </div>
+        <div class="carousel-item" style="height: 100vh">
+          <img src="colombia.jpg" class="d-block w-100 " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Viaja por toda Colombia</h5>
+            <p>Atrévete a explorar todo el territorio colombiano</p>
+          </div>
+        </div>
+        <div class="carousel-item" style="height: 100vh">
+          <img src="dinero.jpg" class="d-block w-100 " alt="...">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Ahorra dinero</h5>
+            <p>Nosotros cubrimos combustible, seguro, parqueo y mantenimiento</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+  <div>
+    <div class="container">
+      <p> </p>
+      <p> </p>
+      <p> </p>
+      <div class="row">
+        <div class="col-md">
+          Membresía
+        </div>
+        <div class="col-md">
+          Rentar  
+        </div>
+        <div class="col-md">
+          Devolver
+        </div>
+      </div>
+    </div>
+    <p> </p>
+    <p> </p>
+    <p> </p>
+    <div class="container">
+      <div class="row">
+        <div class="col-md">
+          <img src="Membresía.jpg">
+        </div>
+        <div class="col-md">
+          <img src="Rentar.jpg">  
+        </div>
+        <div class="col-md">
+          <img src="Devolver.jpg">
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-md">
+          Hazte miembro y obtén nuestra membresía completamente gratis
+        </div>
+        <div class="col-md">
+          Renta ya los mejores autos del mercado cuando quieras y como quieras
+        </div>
+        <div class="col-md">
+          Devolver el auto nunca ha sido tan fácil
+        </div>
+      </div>
+    </div>
+    <p> </p>
+    <p> </p>
+    <p> </p>
+    <div class="footer">
+      <div style="background-image:url(verde.jpg)" class="wrapper">
+          <!-- Estas letras tienen que ir centradas y en la mitad de la imagen verdad, encima de la imagen verde, que no ocupen mucho espacio. Si te das cuenta debajo de la imagen verde hay un espacio en blanco que no se como quitar-->
+          <p>Contactanos</p>
+          <p>Daniel Donado - adavendano@uninorte.edu.co</p>
+          <p>Daniel Valencia - dvalencia@uninorte.edu.co</p>
+          <p>Santiago Zúñiga - sazuniga@uninorte.edu.co</p>
+          <p>Copyright (c) 2020</p>
+      </div>
+    </div>
+   </body>
+</html>
+</div>
+</body>
+</html>
